179e2263ca3213f22eeba88fe30ecde3007b2337
Nathaniel Brost <NathanielBrost@NathaniBook2021.attlocal.net>
2022-01-26 21:05:44 -0600

    first route and schema

---
+++ models/product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// eventually this will change to song, video, playlist, etc:
+const productSchema = new mongoose.Schema({
+    name: {type: String, required: true, minlength: 2, maxlength: 255} ,
+    description: {type: String, required: true} ,
+    category: {type: String, required: true, minlength: 2, maxlength: 50} ,
+    price: {type: Number, required: true} ,
+    dateModified: { type: Date, default: Date.now} , 
+});
+
+const Product = mongoose.model('Product', productSchema);
